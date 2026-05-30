ecc7ba0d10b0f70f80c05c151bcef96846edb904
MBTI Developer <developer@mbti-qtest.dev>
2026-05-30 08:44:06 +0800

    fix: remove lazy loading, direct imports for debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
-import { lazy, Suspense } from 'react';
-import { Spin } from 'antd';
-
-const HomePage = lazy(() => import('./routes/HomePage'));
-const IntroPage = lazy(() => import('./routes/IntroPage'));
-const TestPage = lazy(() => import('./routes/TestPage'));
-const ResultPage = lazy(() => import('./routes/ResultPage'));
-
-function PageLoader() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <Spin size="large" />
-    </div>
-  );
-}
+import HomePage from './routes/HomePage';
+import IntroPage from './routes/IntroPage';
+import TestPage from './routes/TestPage';
+import ResultPage from './routes/ResultPage';
 
 export default function App() {
   const location = useLocation();
 
   return (
-    <Suspense fallback={<PageLoader />}>
-      <div key={location.pathname} className="page-enter-active">
-        <Routes location={location}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/intro" element={<IntroPage />} />
-          <Route path="/test" element={<TestPage />} />
-          <Route path="/result" element={<ResultPage />} />
-        </Routes>
-      </div>
-    </Suspense>
+    <div key={location.pathname} className="page-enter-active">
+      <Routes location={location}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/intro" element={<IntroPage />} />
+        <Route path="/test" element={<TestPage />} />
+        <Route path="/result" element={<ResultPage />} />
+      </Routes>
+    </div>
   );
 }
